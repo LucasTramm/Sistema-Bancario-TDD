43568218713731353049bb60c7d534495df39b0d
LucasTramm <lufreditramm@hotmail.com>
2025-10-10 20:58:24 -0300

    Red-Criação do primeiro teste Saldo Inicial Zero Para Nova Conta

---
+++ banco/src/test/java/ContaTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ContaTest {
+    @Test
+    public void SaldoInicialZeroParaNovoConta() {
+        Conta conta = new Conta("12345");
+        double saldo = conta.getSaldo();
+        assertEquals(0.0, saldo, "O saldo inicial deve ser zero.");      
+    }
+}
